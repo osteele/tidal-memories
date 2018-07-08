a8ada55ac17b30a62bc66b8e5b8ce3b324ad1215
Oliver Steele <steele@osteele.com>
2018-07-08 17:31:56 +0700

    Use JSON.stringify in poi.config.js

--- client/poi.config.js
+++ client/poi.config.js
@@ -3,9 +3,10 @@ const webpack = require('webpack');
 module.exports = {
     entry: './src/index.jsx',
     define: {
-        'process.env.API_SERVER_URL': `"${process.env.API_SERVER_URL ||
-            'http://127.0.0.1:5000/'}"`,
-        'process.env.AUDIO_BASE_URL': `"${process.env.AUDIO_BASE_URL}"`
+        'process.env.API_SERVER_URL': JSON.stringify(
+            process.env.API_SERVER_URL || 'http://127.0.0.1:5000/'
+        ),
+        'process.env.AUDIO_BASE_URL': JSON.stringify(process.env.AUDIO_BASE_URL)
     },
     html: {
         title: 'Dinaconfo — Dinaocon Photo Gallery'
